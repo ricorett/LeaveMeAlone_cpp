9337d35595a5282944c39da6165ff1949b5465cc
Lana <Lana@DESKTOP-2V0AIFS>
2025-02-27 16:06:20 +0300

    Add stamina

---
+++ Source/LeaveMeAlone/Public/Components/LMAAnimInstance.h
@@ -0,0 +1,17 @@
+// LeaveMeAlone Game by Netologiya. All rights are reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "LMAAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEAVEMEALONE_API ULMAAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
